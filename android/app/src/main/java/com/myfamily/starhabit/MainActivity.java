445e1762b37b87084f4c9d682fd69cb21d212a1d
Dinda Priatni Sapa'ah <nurekokustiarno@ruangguru.com>
2025-12-03 11:38:16 +0700

    fix crash build

---
+++ android/app/src/main/java/com/myfamily/starhabit/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myfamily.starhabit;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://vxatejctlhszmwxkzaaf.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4YXRlamN0bGhzem13eGt6YWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1MTE2NzYsImV4cCI6MjA4MDA4NzY3Nn0.B6LMDrMkY7M0TublYVzAfuoMr-TKbl8lYrbhMcasISI'),
+  },
 })
